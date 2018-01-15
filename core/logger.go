ce2b2024e8948abe8aef29df5cfafa0ae2c79886
abhilekhsingh041992 <abhilekhsingh041992@gmail.com>
2018-01-15 17:00:35 +0400

    Common logging file global trace, info, error, warning

---
+++ core/logger.go
@@ -0,0 +1,15 @@
+package core
+
+import (
+	"log"
+	"io/ioutil"
+	"os"
+)
+
+
+var (
+	Trace   = log.New(ioutil.Discard, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Info    = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Warning = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Error   = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+)
